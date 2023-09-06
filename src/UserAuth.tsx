44c7479e40e762db39bf113816fdeeab931c8143
Grace Nieboer <grace.nieboer@outlook.com>
2023-09-06 17:33:42 -0400

    restructure files and create firebase.ts

---
+++ src/UserAuth.tsx
@@ -0,0 +1,17 @@
+// import React, { useContext, useState } from "react";
+
+
+// export const User = React.createContext(null);
+
+// export function UserAuth ({children}) {
+//     const [userAccount, setUserAccount] = useState(null)
+//     const [userToken, setUserToken] = useState(null)
+
+
+
+
+
+//     return (
+//         <UserAuth.Provider>{children}</UserAuth.Provider>
+//     )
+// }
