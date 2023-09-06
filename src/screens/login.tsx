6b146df5e422f0170cc6937b8b012af05e707a6a
Grace Nieboer <grace.nieboer@outlook.com>
2023-09-06 17:36:31 -0400

    add screens, navs, and delete old userauth component

--- src/screens/login.tsx
+++ src/screens/login.tsx
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function LogIn() {
+export default function login() {
   return (
     <View style={styles.container}>
       <Text>LOG IN WILL BE HERE</Text>
