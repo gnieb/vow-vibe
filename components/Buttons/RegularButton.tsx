ccf5f70a6d78103be32c13733f52ef61648d320b
Grace Nieboer <grace.nieboer@outlook.com>
2023-09-27 10:48:42 -0400

    create REgularButton component

---
+++ components/Buttons/RegularButton.tsx
@@ -0,0 +1,17 @@
+import React, {FunctionComponent} from "react";
+import styled from "styled-components/native";
+
+import { colors } from "../colors";
+import RegText from "../Texts/RegText";
+
+const ButtonView = styled.TouchableOpacity`
+`
+
+const RegularButton: FunctionComponent = () => {
+    return (
+        <>
+        </>
+    )
+}
+
+export default RegularButton
