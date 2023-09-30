8102c2a66cac651da03541a633f90a15967b9528
Grace Nieboer <grace.nieboer@outlook.com>
2023-09-30 10:23:30 -0400

    create a linkbutton component and in process of transferring Link into a reusable component

--- components/Screens/Registry.tsx
+++ components/Screens/Registry.tsx
@@ -1,10 +1,44 @@
 import React, {FunctionComponent} from "react";
+import styled from "styled-components/native";
+import { colors } from "../colors";
+import { Container } from "../shared";
+import RegText from "../Texts/RegText";
+import { Link } from "@react-navigation/native";
+
+import { StyleProp, ViewStyle, GestureResponderEvent, TextStyle } from "react-native";
+
+const RegistryContainer = styled(Container)`
+background-color: ${colors.lightgreen};
+width: 100%;
+height:100%;
+flex:1;
+`
+
+const MainSection = styled.View`
+margin: auto;
+`
+
+interface LinkProps {
+    src:string
+    btnStyles?: StyleProp<ViewStyle>;
+    // onPress : ((event: GestureResponderEvent) => void | undefined);
+    textStyles?: StyleProp<TextStyle>;
+    children: React.ReactNode;
+}
+
+const RegistryLink: FunctionComponent<LinkProps> = (props) => {
+    return <Link to={props.src}>{props.children}</Link>
+}
 
 
 const Registry: FunctionComponent = () => {
     return (
-        <>
-        </>
+        <RegistryContainer>
+            <MainSection>
+                <RegText>Go to Wedding Registry</RegText>
+                <RegistryLink  />
+            </MainSection>
+        </RegistryContainer>
     )
 }
 
