6756d448402aa2c42058c6e736c46b92536b688a
Grace Nieboer <grace.nieboer@outlook.com>
2023-10-02 12:29:30 -0400

    add interface and type notes

--- TypeScriptNotes.tsx
+++ TypeScriptNotes.tsx
@@ -33,5 +33,54 @@ let todo:Todo = {
     num:2
 }
 
+// TYPE vs INTERFACE :
+// they work the same. the difference is that types can be extended like this: 
+type A= {
+    a: number;
+    b: number;
+}
+type B = A & {
+    c:string;
+    d:string;
+}
+// this needs the params of both A and B because B is extending A so it includes all of A's params as well.
+let b:B = {
+    a:4,
+    b:5,
+    c:"hi",
+    d:"hello"
+}
+
+// TO extend the interface, we do this:
+
+interface greeting {
+    greet:string
+}
+interface wave extends greeting {
+    isWaving: boolean
+}
+let goodmorning:wave = {
+    greet:"hello",
+    isWaving: true,
+}
+
+
 // array of objects
 let lotsOfTodos: Todo[]
+
+//Q: what if a want a variable to be able to be more than one type?
+// A: use the union symbol: "|"
+let tableNumber: string | number;
+tableNumber="six"
+tableNumber=6
+
+// function types
+//  => this will work fine, but there is a better way that will avoid more errors.
+let printName: Function;
+
+// => this is the better way!!
+let logName: (name:string) => void;
+//      (declare param type) => declare return type;
+
+
+ 
