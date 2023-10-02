b3e9c574833aa35d990bf473c125100fa3a7e9ee
Grace Nieboer <grace.nieboer@outlook.com>
2023-10-02 13:03:34 -0400

    install styled-components jest pacakge

--- components/NewToDo.tsx
+++ components/NewToDo.tsx
@@ -2,6 +2,18 @@ import React from "react";
 import { Button, TextInput, View, StyleSheet } from 'react-native';
 import { Formik } from 'formik';
 import RegularButton from "./Buttons/RegularButton";
+import styled from "styled-components/native";
+import { colors } from "./colors";
+
+
+const InputView = styled.TextInput`
+align-items: center;
+background-color: ${colors.lightgreen};
+width: 80%;
+padding: 20px;
+margin: 10px auto;
+border-radius: 50px;
+`
 
 const NewToDo = () => (
     <Formik
@@ -10,6 +22,7 @@ const NewToDo = () => (
     >
         {({ handleChange, handleBlur, handleSubmit, values }) => (
        <View>
+        <InputView>
          <TextInput
            onChangeText={handleChange('name')}
            onBlur={handleBlur('name')}
@@ -17,6 +30,7 @@ const NewToDo = () => (
            placeholder="what's next on the list?"
            
          />
+         </InputView>
          <RegularButton onPress={handleSubmit} title="Submit">Submit</RegularButton>
        </View>
      )}
