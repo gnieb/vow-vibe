b75ab9ca521d9a9780e0b36907a26799a0e4c70c
Grace Nieboer <grace.nieboer@outlook.com>
2023-10-01 23:37:08 -0400

    creste ToDo component

---
+++ components/Screens/Todos.tsx
@@ -0,0 +1,20 @@
+import React, {FunctionComponent, useState} from "react";
+import styled from "styled-components/native";
+import { colors } from "../colors";
+
+
+
+
+const Todos: FunctionComponent = () => {
+const [todos, setTodos] = useState(['this', 'that', 'the other thing'])
+
+
+const displayTodos = todos.map(t => {
+    
+})
+
+    return (
+        <>
+        </>
+    )
+}
---
+++ components/ToDo.tsx
@@ -0,0 +1,10 @@
+import React, {FunctionComponent} from "react";
+import styled from "styled-components/native";
+
+const ToDo: FunctionComponent = () => {
+    return (
+        <>
+        
+        </>
+    )
+}
