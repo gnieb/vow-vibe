f64f37fd4abc23ef7cc9cc7bef146029b4887c5d
Grace Nieboer <grace.nieboer@outlook.com>
2023-10-09 11:05:20 -0400

    write useEffect to Todos to test the localhost api call

--- components/Screens/Todos.tsx
+++ components/Screens/Todos.tsx
@@ -15,27 +15,25 @@ align-items: center;
 justifyContent: center;
 `
 
-// const getToDos = async (url:string): Promise<Array<Todo> | string > => {
-//     try {
-//         const response = await fetch(url)
-//         const { data } = await response.json()
-//         return data
-//     } catch (error) {
-//         if (error) {
-//             return error.message
-//         }
-//     }
-// }
+const getToDos = async (url:string): Promise<any > => {
+    try {
+        const response = await fetch(url)
+        const { data } = await response.json()
+        return data
+    } catch (error) {
+        if (error) {
+            console.log("oh no", error)
+        }
+    }
+}
 
 const Todos: FunctionComponent = () => {
 const [todos, setTodos] = useState<ToDo[]>([{id:1, todo:"this", isDone:false}, {id:2, todo:"that", isDone:false}, {id:3, todo:"the other thing", isDone:false}])
 
-// useEffect(async () => {
-//     const response = await fetch(
-//         "https://jsonplaceholder.typicode.com/todos"
-//       )
-//       const body = await response.json()
-// })
+useEffect(() => {
+    getToDos("")
+}, [])
+
 
     return (
         <TodosContainer>
