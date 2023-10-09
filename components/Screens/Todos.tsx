7e6183d949fbf94c648e2cddae63899a86dea682
Grace Nieboer <grace.nieboer@outlook.com>
2023-10-09 10:59:33 -0400

    Revert "adding .env file  variable to access ip"
    This reverts commit 4cd679cfb376a00813ab4b3e00a3264608c2b409.

--- components/Screens/Todos.tsx
+++ components/Screens/Todos.tsx
@@ -6,7 +6,6 @@ import { Container } from "../shared";
 import ToDo from "../ToDo";
 import ToDoList from "../ToDoList";
 
-
 const TodosContainer = styled(Container)`
 background-color: ${colors.darkgreen};
 width: 100%;
@@ -16,29 +15,27 @@ align-items: center;
 justifyContent: center;
 `
 
-const devUrl = process.env.EXPO_PUBLIC_IP;
-console.log(devUrl, "hello")
-
-const getToDos = async (url:string): Promise<any > => {
-    try {
-        const response = await fetch(url)
-        const data = await response.json()
-        console.log("this is good")
-        return data
-    } catch (error) {
-        if (error) {
-            console.log("oh no!")
-            console.log(error)
-        }
-    }
-}
+// const getToDos = async (url:string): Promise<Array<Todo> | string > => {
+//     try {
+//         const response = await fetch(url)
+//         const { data } = await response.json()
+//         return data
+//     } catch (error) {
+//         if (error) {
+//             return error.message
+//         }
+//     }
+// }
 
 const Todos: FunctionComponent = () => {
 const [todos, setTodos] = useState<ToDo[]>([{id:1, todo:"this", isDone:false}, {id:2, todo:"that", isDone:false}, {id:3, todo:"the other thing", isDone:false}])
 
-useEffect(() => {
-    getToDos("/users/1")
-}, [])
+// useEffect(async () => {
+//     const response = await fetch(
+//         "https://jsonplaceholder.typicode.com/todos"
+//       )
+//       const body = await response.json()
+// })
 
     return (
         <TodosContainer>
