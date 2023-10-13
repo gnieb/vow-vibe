d962b6023386f397d15c94c81e53525634fcb1d2
Grace Nieboer <grace.nieboer@outlook.com>
2023-10-13 10:04:11 -0400

    add Flatlist and useState for the GuestList component

--- components/Screens/GuestList.tsx
+++ components/Screens/GuestList.tsx
@@ -1,18 +1,31 @@
-import React, {FunctionComponent} from "react";
+import React, {useState, useEffect, FunctionComponent} from "react";
 import styled from "styled-components/native";
-import { View, Text } from "react-native";
+import { View, Text, FlatList } from "react-native";
+import { colors } from "../colors";
+import Guest from "../Guest";
 
+
+const GuestView = styled.View`
+background-color:${colors.mediumgreen};
+width:100%;
+border-radius: 50px;
+padding:10px;
+margin-top:15px;
+justifyContent:center;
+`
 
 const GuestList:FunctionComponent = () => {
-
-    
+    const [guests, setGuests] = useState<Guest[]>([])
 
 
 
     return (
-        <View>
+        <GuestView>
             <Text>PEOPLE YOU  LOVE HERE!!!</Text>
-        </View>
+            <FlatList data={guests} 
+            renderItem={}/>
+            {/* create a lil guest list item to be displayed */}
+        </GuestView>
     )
 }
 
