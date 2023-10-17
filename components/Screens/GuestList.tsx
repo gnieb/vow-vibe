f50fb004e3183b66a45b6294b2cd64e919c56d8d
Grace Nieboer <grace.nieboer@outlook.com>
2023-10-16 22:48:06 -0400

    style the list text

--- components/Screens/GuestList.tsx
+++ components/Screens/GuestList.tsx
@@ -17,6 +17,16 @@ margin-top:15px;
 justifyContent:center;
 `
 
+const GuestListTitleView = styled.View`
+background-color:${colors.mediumgreen};
+width:100%;
+text-align: center;
+border-radius: 50px;
+padding:20px;
+margin-top:15px;
+justifyContent:center;
+`
+
 const GuestContainer = styled(Container)`
 background-color: ${colors.darkgreen};
 width: 100%;
@@ -26,6 +36,8 @@ align-items: center;
 justifyContent: center;
 `
 
+
+
 const GuestList:FunctionComponent = () => {
     const [guests, setGuests] = useState<Guest[]>([{id:1, wedding_id:1, first_name:"Abby", last_name:"Knowlton"}, {id:2, wedding_id:1, first_name:"Bryant", last_name:"Knowlton"}])
 
@@ -33,9 +45,9 @@ const GuestList:FunctionComponent = () => {
 
     return (
         <GuestContainer >
-            <GuestView>
-                <RegText>THE LIST</RegText>
-            </GuestView>
+            <GuestListTitleView>
+                <RegText textStyles={{color:"white", textAlign:"center", fontSize:30, fontFamily:"Helvetica"}}>WHO'S COMING</RegText>
+            </GuestListTitleView>
             <GuestView>
                 <FlatList data={guests} 
                 renderItem={({item}) => <GuestListItem item={item} key={item.id} guests={guests} setGuests={setGuests} />}/>
