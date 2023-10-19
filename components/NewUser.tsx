687f45cd758160e02822ab56324c6563477edd7d
Grace Nieboer <grace.nieboer@outlook.com>
2023-10-19 16:03:29 -0400

    create form with Formik and Yup validation schema to create a new user with no state changes yet, just console logs

--- components/NewUser.tsx
+++ components/NewUser.tsx
@@ -5,7 +5,7 @@ import RegularButton from "./Buttons/RegularButton";
 import styled from "styled-components/native";
 import { colors } from "./colors";
 import RegText from "./Texts/RegText";
-import ToDo from "./ToDo";
+import { User } from "./User";
 import { useState } from "react";
 import * as Yup from 'yup';
 import uuid from 'react-native-uuid';
@@ -13,8 +13,20 @@ import uuid from 'react-native-uuid';
 let initialValues = {
     first_name: "",
     last_name:"",
-    
+    email:"",
 }
+
+const ToDoSchema = Yup.object().shape({
+    first_name: Yup.string()
+      .min(2, 'Too Short!')
+      .required('Required'),
+    last_name: Yup.string()
+    .min(2, 'Too Short!')
+    .required('Required!'),
+    email: Yup.string()
+    .email('Invalid Email').required('Required!')
+  });
+
 
 const NewUser:FunctionComponent = () => {
     return (
@@ -23,28 +35,56 @@ const NewUser:FunctionComponent = () => {
         validationSchema={ToDoSchema}
         onSubmit={(val, {resetForm}) => {
             console.log(val)
-            const newTask:ToDo = {id: Date.now(), isDone:false, todo: val.name,}
-            setTodos(todos => [...todos, newTask])
+            const newUser:User = {email:val.email, first_name: val.first_name, last_name: val.last_name, todos: []}
+            // do something with the newUser:
+            // POST new User to server/database
+            console.log(newUser)
+
             resetForm({values: initialValues})
         }}
     >
         {({ handleChange, handleBlur, handleSubmit, values, errors, touched  }) => (
        <View style={formStyles.formContainer}>
      
-         <RegText textStyles={formStyles.title}>What's Next on the List?</RegText>
+         <RegText textStyles={formStyles.title}>SIGN UP TO START PLANNING THE BEST DAY OF YOUR LIFE</RegText>
          <View style={formStyles.inputWrapper}>
             <TextInput
-            onChangeText={handleChange('name')}
-            onBlur={handleBlur('name')}
-            value={values.name}
-            placeholder="add it to the pile..."
+            onChangeText={handleChange('first_name')}
+            onBlur={handleBlur('first_name')}
+            value={values.first_name}
+            placeholder="first name..."
             style={formStyles.inputStyle}
             />
          </View>
-         {errors.name && touched.name ? (
-             <RegText>{errors.name}</RegText>
+         {errors.first_name && touched.first_name ? (
+             <RegText>{errors.first_name}</RegText>
            ) : null}
-         <RegularButton  onPress={()=> handleSubmit()} >Submit</RegularButton>
+           <View style={formStyles.inputWrapper}>
+            <TextInput
+            onChangeText={handleChange('last_name')}
+            onBlur={handleBlur('last_name')}
+            value={values.last_name}
+            placeholder="last name ..."
+            style={formStyles.inputStyle}
+            />
+         </View>
+         {errors.last_name && touched.last_name ? (
+             <RegText>{errors.last_name}</RegText>
+           ) : null}
+           <View style={formStyles.inputWrapper}>
+            <TextInput
+            onChangeText={handleChange('email')}
+            onBlur={handleBlur('email')}
+            value={values.email}
+            placeholder="email.."
+            style={formStyles.inputStyle}
+            />
+         </View>
+         {errors.email && touched.email ? (
+             <RegText>{errors.email}</RegText>
+           ) : null}
+
+         <RegularButton  onPress={()=> handleSubmit()} >Sign up!</RegularButton>
 
        </View>
      )}
