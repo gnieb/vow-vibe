d0eb7deae380bb57ae5e0fd3ab6009697c66e99d
Grace Nieboer <grace.nieboer@outlook.com>
2023-10-19 16:05:52 -0400

    create Sign Up screen container and import new user form.

--- components/Screens/SignUp.tsx
+++ components/Screens/SignUp.tsx
@@ -1,11 +1,21 @@
 import React, { FunctionComponent } from "react"
 import { SafeAreaView, View, Text } from "react-native"
+import NewUser from "../NewUser"
+import styled from "styled-components/native"
+import { colors } from "../colors"
+import { Container } from "../shared"
+
+const SignUpContainer = styled(Container)`
+background-color: ${colors.darkgreen};
+width: 100%;
+flex:1;
+`
 
 const SignUp:FunctionComponent = () => {
 return (
-    <>
-    <Text>HI, Sign up</Text>
-    </>
+    <SignUpContainer>
+        <NewUser />
+    </SignUpContainer>
 )
 }
 
