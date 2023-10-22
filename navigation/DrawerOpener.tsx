8be1316edf1af4862e77bf774aa56e82d8c37cea
Grace Nieboer <grace.nieboer@outlook.com>
2023-10-22 12:39:45 -0400

    create state for drawer isOpen set to a boolean. when icon is pushed, isOpen is logged to the console.

--- navigation/DrawerOpener.tsx
+++ navigation/DrawerOpener.tsx
@@ -4,6 +4,7 @@ import styled from "styled-components/native";
 import { Container } from "../components/shared";
 import { colors } from "../components/colors";
 import { TouchableHighlight } from "react-native-gesture-handler";
+import { useState } from "react";
 
 
 
@@ -15,18 +16,24 @@ padding-top:40px;
 padding-left:20px;
 `
 
+// need to set state assigned to drawer open vs closed... 
+
+
+
+
 const DrawerOpener = () => {
+const [isOpen, setIsOpen] = useState(false)
+
     return (
         <MenuContainer>
-                
-
-            <TouchableHighlight onPress={()=>{console.log("pushed!")}}>
-            <View>
-                <Entypo name="menu" size={35} color="black" />
-            </View>
+            <TouchableHighlight onPress={()=> {
+                setIsOpen(!isOpen) 
+                console.log(isOpen)
+                }}>
+                <View>
+                    <Entypo name="menu" size={35} color="black" />
+                </View>
             </TouchableHighlight>    
-                
-            
         </MenuContainer>
     )
 }
