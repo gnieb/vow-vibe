5199892d93c7eef8e6e0732bcfc3ce0005b72c8d
Grace Nieboer <grace.nieboer@outlook.com>
2023-10-23 14:28:02 -0400

    add the DrawerOpener to Homescreen

--- components/Screens/HomeScreen.tsx
+++ components/Screens/HomeScreen.tsx
@@ -3,6 +3,7 @@ import styled from "styled-components/native";
 import { Container } from "../shared";
 import { colors } from "../colors";
 import { StatusBar } from "expo-status-bar";
+import DrawerOpener from "../../navigation/DrawerOpener";
 
 const HomeScreenContainer = styled(Container)`
 background-color: ${colors.darkgreen};
@@ -10,11 +11,14 @@ width: 100%;
 flex:1;
 `
 
-const HomeScreen: FunctionComponent = () => {
+const HomeScreen: FunctionComponent = ({navigation}:any) => {
     return (
+        <>
+        <DrawerOpener navigation={navigation} />
         <HomeScreenContainer>
             <StatusBar style="light"/>
         </HomeScreenContainer>
+        </>
     )
 }
 
