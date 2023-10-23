e1c23aa526cf3cfb354713ebb4a3854e22681d1b
Grace Nieboer <grace.nieboer@outlook.com>
2023-10-23 14:29:09 -0400

    add DrawerOpener component to the Login Screen

--- components/Screens/login.tsx
+++ components/Screens/login.tsx
@@ -1,13 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { FunctionComponent } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import DrawerOpener from '../../navigation/DrawerOpener';
 
-const Login:FunctionComponent = () => {
+const Login:FunctionComponent = ({navigation}:any) => {
   return (
+
+    <>
+    <DrawerOpener navigation={navigation} />
     <View style={styles.container}>
       <Text>LOG IN WILL BE HERE</Text>
       <StatusBar style="auto" />
     </View>
+    </>
   );
 }
 
