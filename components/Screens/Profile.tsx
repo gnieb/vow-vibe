ea9bf055b87c6eff3308a75757e4bdf9692c0295
Grace Nieboer <grace.nieboer@outlook.com>
2023-10-25 21:46:20 -0400

    create mthe ptofile component with just one view and text

---
+++ components/Screens/Profile.tsx
@@ -0,0 +1,17 @@
+
+
+
+import React, {FunctionComponent} from "react";
+import { Text, View } from "react-native";
+
+
+
+const Profile:FunctionComponent = () => {
+    return (
+        <View>
+            <Text>PROFILE HERE</Text>
+        </View>
+    )
+}
+
+export default Profile;
