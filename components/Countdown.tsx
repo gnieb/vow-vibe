551c46a381cc68a827b10deb684ecd3909c43079
Grace Nieboer <grace.nieboer@outlook.com>
2023-11-01 09:58:52 -0400

    create basic countdown component

---
+++ components/Countdown.tsx
@@ -0,0 +1,11 @@
+import { FunctionComponent } from "react";
+import { Text } from "react-native";
+
+
+const Countdown:FunctionComponent = () => {
+    return (
+        <Text>COUNTDOWN HERE</Text>
+    )
+}
+
+export default Countdown;
