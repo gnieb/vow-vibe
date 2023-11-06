cb5cfe82dd26cfc29174e854e167c33a00d0a8ce
Grace Nieboer <grace.nieboer@outlook.com>
2023-11-05 19:23:47 -0500

    create new wedding component which will hold a form that stores a new wedding date

---
+++ components/NewWedding.tsx
@@ -0,0 +1,13 @@
+import { FunctionComponent } from "react";
+import { View, Text } from "react-native";
+
+
+const NewWedding:FunctionComponent = () => {
+    return (
+        <View>
+            <Text>Wedding form here</Text>
+        </View>
+    )
+}
+
+export default NewWedding;
