86f54c6d9e308e9f105ffa826242cab828cd3ae6
Grace Nieboer <grace.nieboer@outlook.com>
2023-11-18 12:04:46 -0500

    Guest List pulls all guests associated with this user. Would like to also have a total guest count displayed...

--- components/Screens/GuestList.tsx
+++ components/Screens/GuestList.tsx
@@ -8,6 +8,7 @@ import RegText from "../Texts/RegText";
 import { Container } from "../shared";
 import NewGuest from "../NewGuest";
 import DrawerOpener from "../../navigation/DrawerOpener";
+import { useAuth } from "../../context/AuthContext";
 
 // background-color: white;
 const GuestView = styled.View`
@@ -22,7 +23,7 @@ justifyContent:center;
 
 
 const GuestContainer = styled(Container)`
-background-color: white;
+background-color: #e0e0e0;
 width: 100%;
 height:100%;
 flex:1;
@@ -31,11 +32,28 @@ justifyContent: center;
 `
 
 
+const API_URL = "http://192.168.1.6:5555"
 
 const GuestList:FunctionComponent = ({navigation}:any) => {
+    const {user} = useAuth()
     const [guests, setGuests] = useState<Guest[]>([{id:1, user_id:1, first_name:"Abby", last_name:"Knowlton"}, {id:2, user_id:1, first_name:"Bryant", last_name:"Knowlton"}])
-    
     const addNew = (newG:Guest) => {setGuests((guests) => [...guests, newG])}
+
+    useEffect(() => {
+        const getGuests = async () => {
+            try {
+                const response = await fetch(`${API_URL}/users/${user?.id}`)
+                const data = await response.json()
+
+                setGuests(data.guests)
+
+            } catch (e) {
+                return {error: true, msg:(e as any).response.data.msg}
+            } 
+        }
+
+        getGuests()
+    }, [])
 
     return (
         <>
